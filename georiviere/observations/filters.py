ee3224bc0883bf8ae740bc8d1f2ca4eff8d67b84
ehe <ehe@makina-corpus.com>
2021-11-17 16:52:53 +0100

    Fix filter on station

--- georiviere/observations/filters.py
+++ georiviere/observations/filters.py
@@ -23,7 +23,7 @@ class StationFilterSet(WatershedFilterSet, ZoningFilterSet, MapEntityFilterSet):
         queryset=Parameter.objects.all(),
     )
     in_service = ChoiceFilter(
-        null_label=_('Unknown'),
+        empty_label=_('Unknown'),
         choices=choices
     )
 
