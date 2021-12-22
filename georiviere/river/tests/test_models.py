65a000a3cb3b610ede29f5ffd0d18fc01f6b45e4
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2021-12-22 11:44:48 +0100

    Fix tests in river
    str morpho and status are tested in description app

--- georiviere/river/tests/test_models.py
+++ georiviere/river/tests/test_models.py
@@ -13,8 +13,8 @@ class TopologyTest(TestCase):
         morphology = MorphologyFactory.create()
         status = StatusFactory.create()
         lonely_topology = TopologyFactory()
-        self.assertEqual(str(status.topology), "Status {}".format(status.pk))
-        self.assertEqual(str(morphology.topology), "Morphology {}".format(morphology.pk))
+        self.assertEqual(str(status.topology), "Status {}".format(str(status)))
+        self.assertEqual(str(morphology.topology), "Morphology {}".format(str(morphology)))
         self.assertEqual(str(lonely_topology), "Topology {}".format(lonely_topology.pk))
 
 
