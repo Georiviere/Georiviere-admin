887ca5825e8de3e916bbb1416572b2a171ea42a3
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-13 18:48:50 +0100

    Add object param to template_tag

--- georiviere/main/templatetags/georiviere_tags.py
+++ georiviere/main/templatetags/georiviere_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('main/_detail_valuelist_source_fragment.html')
-def valuelist_source(items, field=None, enumeration=False):
+def valuelist_source(items, object=None, field=None, enumeration=False):
     """
     Common template tag to show a list of values in detail pages.
 
@@ -27,13 +27,17 @@ def valuelist_source(items, field=None, enumeration=False):
 
     valuelist = []
     for i, item in enumerate(items):
-        distance_to_source = None
         if field:
             text = getattr(item, '%s_display' % field, getattr(item, field))
         else:
             text = item
-        if modelname == 'stream':
-            distance_to_source = item.distance_to_source
+        if object:
+            if modelname == 'stream':
+                distance_to_source = item.distance_to_source
+            else:
+                distance_to_source = object.distance_to_source
+        else:
+            distance_to_source = None
         valuelist.append({
             'enumeration': letters[i] if enumeration else False,
             'pk': getattr(items[i], 'pk', None),
--- georiviere/main/tests/test_templatetags.py
+++ georiviere/main/tests/test_templatetags.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.utils import translation
 
 from georiviere.river.tests.factories import StreamFactory
+from georiviere.knowledge.tests.factories import KnowledgeFactory
 
 
 class ValueListTest(TestCase):
@@ -10,13 +11,15 @@ class ValueListTest(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         translation.deactivate()
-        cls.obj = StreamFactory(name='blah')
+        cls.stream = StreamFactory(name='A river')
+        cls.knowledge = KnowledgeFactory(name='A bridge')
 
     def test_empty_list_should_show_none(self):
         out = Template(
             '{% load georiviere_tags %}'
-            '{% valuelist_source items %}'
+            '{% valuelist_source items object %}'
         ).render(Context({
+            'object': '',
             'items': []
         }))
         self.assertHTMLEqual(out.strip(), '<span class="none">None</span>')
@@ -24,28 +27,43 @@ class ValueListTest(TestCase):
     def test_simple_usage_outputs_list_of_items(self):
         out = Template(
             '{% load georiviere_tags %}'
-            '{% valuelist_source items %}'
+            '{% valuelist_source items object %}'
         ).render(Context({
+            'object': '',
             'items': ['blah']
         }))
         self.assertHTMLEqual(out.strip(), """<ul><li>blah</li></ul>""")
 
-    def test_obj_with_distance_to_source(self):
+    def test_obj_list_with_distance_to_source_related_to_stream(self):
         out = Template(
             '{% load georiviere_tags %}'
-            '{% valuelist_source items %}'
+            '{% valuelist_source items object %}'
         ).render(Context({
-            'items': [self.obj]
+            'object': self.stream,
+            'items': [self.knowledge]
         }))
         self.assertHTMLEqual(out.strip(), f"""<ul>
-        <li class="hoverable" data-modelname="stream" data-pk="{self.obj.pk}">{self.obj.name} (42m)</li>
+        <li class="hoverable" data-modelname="knowledge" data-pk="{self.knowledge.pk}">{self.knowledge.name} (42m)</li>
+        </ul>""")
+
+    def test_stream_list_with_distance_to_source_related_to_object(self):
+        out = Template(
+            '{% load georiviere_tags %}'
+            '{% valuelist_source items object %}'
+        ).render(Context({
+            'object': self.knowledge,
+            'items': [self.stream]
+        }))
+        self.assertHTMLEqual(out.strip(), f"""<ul>
+        <li class="hoverable" data-modelname="stream" data-pk="{self.stream.pk}">{self.stream.name} (42m)</li>
         </ul>""")
 
     def test_can_specify_an_enumeration4(self):
         out = Template(
             '{% load georiviere_tags %}'
-            '{% valuelist_source items enumeration=True %}'
+            '{% valuelist_source items object enumeration=True %}'
         ).render(Context({
+            'object': '',
             'items': range(1, 4)
         }))
         self.assertInHTML('<li><span class="enumeration-value">A.&nbsp;</span>1</li>', out)
