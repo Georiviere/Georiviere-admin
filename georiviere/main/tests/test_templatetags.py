eb58e3689fa1038b3336f21195639738d3ff8576
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-19 17:18:33 +0100

    Fix tests

--- georiviere/main/tests/test_templatetags.py
+++ georiviere/main/tests/test_templatetags.py
@@ -14,13 +14,6 @@ class ValueListTest(TestCase):
         translation.deactivate()
         cls.stream = StreamFactory(name='A river')
         cls.knowledge = KnowledgeFactory(name='A bridge')
-        cls.mock_distance_to_source_patcher = mock.patch('georiviere.river.models.Stream.distance_to_source')
-        cls.mock_distance_to_source = cls.mock_distance_to_source_patcher.start()
-        cls.mock_distance_to_source.return_value = 42.63
-
-    @classmethod
-    def teardown_class(cls):
-        cls.mock_distance_to_source_patcher.stop()
 
     def test_empty_list_should_show_none(self):
         out = Template(
@@ -32,7 +25,9 @@ class ValueListTest(TestCase):
         }))
         self.assertHTMLEqual(out.strip(), '<span class="none">None</span>')
 
-    def test_obj_list_with_distance_to_source_related_to_stream(self):
+    @mock.patch('georiviere.river.models.Stream.distance_to_source')
+    def test_obj_list_with_distance_to_source_related_to_stream(self, mock_distance_to_source):
+        mock_distance_to_source.return_value = 42.63
         out = Template(
             '{% load georiviere_tags %}'
             '{% valuelist_source items stream %}'
@@ -45,7 +40,9 @@ class ValueListTest(TestCase):
         (42.6 m)</li>
         </ul>""")
 
-    def test_obj_list_with_distance_to_source_related_to_stream_with_field(self):
+    @mock.patch('georiviere.river.models.Stream.distance_to_source')
+    def test_obj_list_with_distance_to_source_related_to_stream_with_field(self, mock_distance_to_source):
+        mock_distance_to_source.return_value = 42.63
         out = Template(
             '{% load georiviere_tags %}'
             '{% valuelist_source items stream "name" %}'
@@ -60,7 +57,9 @@ class ValueListTest(TestCase):
         (42.6 m)</li>
         </ul>""")
 
-    def test_stream_list_with_distance_to_source_related_to_object(self):
+    @mock.patch('georiviere.river.models.Stream.distance_to_source')
+    def test_stream_list_with_distance_to_source_related_to_object(self, mock_distance_to_source):
+        mock_distance_to_source.return_value = 42.63
         out = Template(
             '{% load georiviere_tags %}'
             '{% valuelist_streams streams object %}'
