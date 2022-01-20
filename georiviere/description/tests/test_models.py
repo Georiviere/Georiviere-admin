9bb07b32d5a5b3aeb12ba2a24abb1bad0f8f498a
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-20 11:15:19 +0100

    Fix tests

--- georiviere/description/tests/test_models.py
+++ georiviere/description/tests/test_models.py
@@ -46,8 +46,8 @@ class MorphologyTest(TestCase):
             main_flow=factories.FlowTypeFactory.create(label="Plat"),
             full_edge_width=15.0,
         )
-        self.assertEqual(str(morpho1), "- / 0.0")
-        self.assertEqual(str(morpho2), "Plat / 15.0")
+        self.assertEqual(str(morpho1), "None")
+        self.assertEqual(str(morpho2), "Plat")
 
     def test_planlayout_str(self):
         plan_layout_type = factories.PlanLayoutTypeFactory.create()
