2eb37aa2f266687707f83a2f7a91e9c752e6ecda
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-03-07 16:50:37 +0100

    Fix flake

--- georiviere/description/forms.py
+++ georiviere/description/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field
+from crispy_forms.layout import Field
 from crispy_forms.bootstrap import AppendedText
 
 from geotrek.common.forms import CommonForm
