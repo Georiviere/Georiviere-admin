754f32a81d705d2ee37c847ba24d40f3ad0b6a29
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-03-09 14:49:03 +0100

    Improve knowledge layer

--- georiviere/knowledge/static/knowledge/js/main.js
+++ georiviere/knowledge/static/knowledge/js/main.js
@@ -1,23 +1,31 @@
 //
 // Core
 //
+
 $(window).on('entity:map', function (e, data) {
+    var modelname = 'knowledge';
+    var layername = `${modelname}_layer`;
+    var url = window.SETTINGS.urls[layername];
+    var loaded_knowledge = false;
     var map = data.map;
 
-    // Show infrastructure layer in application maps
-	var layer = new L.ObjectsLayer(null, {
-		modelname: 'knowledge',
-		style: L.Util.extend(window.SETTINGS.map.styles['knowledge'] || {}, { clickable:false }),
-		pointToLayer: function (feature, latlng) {
-			var knowledgeIcon = L.icon({
-				iconUrl: feature.properties.type.pictogram,
-				iconSize: [18, 18],
-				iconAnchor: [9, 9],
-			});
-			return L.marker(latlng, {icon: knowledgeIcon});
-		}
-	});
-	var url = window.SETTINGS.urls['knowledge_layer'];
-	layer.load(url);
-	map.layerscontrol.addOverlay(layer, tr('Knowledges'), tr('Knowledges'));
+    // Show station layer in application maps
+       var layer = new L.ObjectsLayer(null, {
+               modelname: modelname,
+               style: L.Util.extend(window.SETTINGS.map.styles[modelname] || {}, { clickable:false }),
+       });
+
+    if (data.modelname != modelname){
+           map.layerscontrol.addOverlay(layer, tr('Knowledge'), tr('Knowledges'));
+    };
+
+    map.on('layeradd', function (e) {
+        var options = e.layer.options || { 'modelname': 'None' };
+        if (! loaded_knowledge) {
+            if (options.modelname == modelname && options.modelname != data.modelname) {
+                e.layer.load(url);
+                loaded_knowledge = true;
+            }
+        }
+    });
 });
