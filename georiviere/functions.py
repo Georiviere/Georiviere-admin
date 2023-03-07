0d78073f4816f3b325e4056f60021256057f4c2d
LePetitTim <timothee.de-montety@makina-corpus.com>
2023-03-07 10:38:15 +0100

    ✨ Add functions Closestpoint / linelocatepoint/ length

--- georiviere/functions.py
+++ georiviere/functions.py
@@ -4,6 +4,20 @@ from django.db.models import FloatField
 from georiviere.fields import ElevationInfosField
 
 
+class ClosestPoint(GeomOutputGeoFunc):
+    geom_param_pos = (0, 1)
+
+
+class LineLocatePoint(GeoFunc):
+    geom_param_pos = (0, 1)
+    output_field = FloatField()
+
+
+class Length(GeoFunc):
+    """ ST_Length postgis function """
+    output_field = FloatField()
+
+
 class ElevationInfos(GeoFunc):
     function = 'ft_elevation_infos'
     geom_param_pos = (0, )
