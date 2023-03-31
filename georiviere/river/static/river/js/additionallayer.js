1ae1bc4c598cd47f17f64801b8c0c46078fe2a30
LePetitTim <timothee.de-montety@makina-corpus.com>
2023-03-31 10:05:05 +0200

    ✨ Add additionnallayer js

---
+++ georiviere/river/static/river/js/additionallayer.js
@@ -0,0 +1,70 @@
+$(window).on('detailmap:ready', function (e, data) {
+    var map = data.map;
+    var mapViewContext = getURLParameter('context');
+
+    if (mapViewContext && mapViewContext.additional_objects) {
+        if (mapViewContext.additional_objects.includes('usages')) {
+            var geojsonUsageMarkerOptions = {
+                radius: 13,
+                fillColor: "#64340c",
+                color: "#64340c",
+                weight: 3,
+                opacity: 1,
+                fillOpacity: 0.4
+            };
+            $.getJSON(window.SETTINGS.urls.stream_usages_layer, function (data) {
+                var usages = new L.GeoJSON(data, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, geojsonUsageMarkerOptions);
+                    }
+                });
+                map.addLayer(usages);
+
+                usages.showEnumeration();
+                $('.map-panel').addClass('other_object_enum_loaded');
+            });
+        }
+        if (mapViewContext.additional_objects.includes('studies')) {
+            var geojsonStudyMarkerOptions = {
+                radius: 13,
+                fillColor: "#d43484",
+                color: "#d43484",
+                weight: 3,
+                opacity: 1,
+                fillOpacity: 0.4
+            };
+            $.getJSON(window.SETTINGS.urls.stream_studies_layer, function (data) {
+                var usages = new L.GeoJSON(data, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, geojsonStudyMarkerOptions);
+                    }
+                });
+                map.addLayer(usages);
+
+                usages.showEnumeration();
+                $('.map-panel').addClass('other_object_enum_loaded');
+            });
+        }
+        if (mapViewContext.additional_objects.includes('studies')) {
+            var geojsonStudyMarkerOptions = {
+                radius: 13,
+                fillColor: "#d43484",
+                color: "#d43484",
+                weight: 3,
+                opacity: 1,
+                fillOpacity: 0.4
+            };
+            $.getJSON(window.SETTINGS.urls.stream_studies_layer, function (data) {
+                var usages = new L.GeoJSON(data, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, geojsonStudyMarkerOptions);
+                    }
+                });
+                map.addLayer(usages);
+
+                usages.showEnumeration();
+                $('.map-panel').addClass('other_object_enum_loaded');
+            });
+        }
+    }
+});
