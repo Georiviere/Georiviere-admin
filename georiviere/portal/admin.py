42d382fad2398d098c1569b0c02b9302d8ff52b6
LePetitTim <timothee.de-montety@makina-corpus.com>
2023-05-03 17:30:37 +0200

    ✨ Add base layers informations

--- georiviere/portal/admin.py
+++ georiviere/portal/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
-from georiviere.portal.models import Portal
+from leaflet.admin import LeafletGeoAdmin
+
+from georiviere.portal.models import MapBaseLayer, MapOverlayLayer, MapOverlayGroupLayer, Portal
 
 
-class PortalAdmin(admin.ModelAdmin):
+class PortalAdmin(LeafletGeoAdmin, admin.ModelAdmin):
     list_display = ('name', 'website', 'title')
     search_fields = ('name', 'website')
 
 
+class MapBaseLayerAdmin(admin.ModelAdmin):
+    list_display = ('label', 'url')
+    search_fields = ('label', )
+
+
+class MapOverlayGroupLayerAdmin(admin.ModelAdmin):
+    list_display = ('label', )
+    search_fields = ('label', )
+
+
+class MapOverlayLayerAdmin(admin.ModelAdmin):
+    list_display = ('label', 'url')
+    search_fields = ('label', )
+
+
 admin.site.register(Portal, PortalAdmin)
+admin.site.register(MapBaseLayer, MapBaseLayerAdmin)
+admin.site.register(MapOverlayGroupLayer, MapOverlayGroupLayerAdmin)
+admin.site.register(MapOverlayLayer, MapOverlayLayerAdmin)
--- georiviere/portal/models.py
+++ georiviere/portal/models.py
@@ -1,9 +1,62 @@
 from colorfield.fields import ColorField
+from django.conf import settings
+from django.contrib.gis.db.models import PolygonField
 from django.db import models
 
 from django.utils.translation import gettext_lazy as _
 
 from geotrek.common.mixins import TimeStampedModelMixin
+
+
+class MapBaseLayer(models.Model):
+    label = models.CharField(max_length=50, unique=True)
+    order = models.PositiveSmallIntegerField(default=0)
+    url = models.CharField(max_length=255, blank=True, help_text=_("URL"))
+    min_zoom = models.PositiveSmallIntegerField(default=0)
+    max_zoom = models.PositiveSmallIntegerField(default=22)
+    attribution = models.CharField(max_length=255, blank=True,
+                                   help_text=_("Attribution of the baselayer. Example : '© OpenStreetMap"))
+    bounds = models.CharField(max_length=255, blank=True, help_text=_("Bounds"))
+
+    class Meta:
+        verbose_name = _("Map base layer")
+        verbose_name_plural = _("Map base layers")
+        ordering = ('label',)
+
+    def __str__(self):
+        return self.label
+
+
+class MapOverlayGroupLayer(models.Model):
+    label = models.CharField(max_length=50)
+    order = models.PositiveSmallIntegerField(default=0)
+    layers = models.ManyToManyField('portal.MapOverlayLayer',
+                                    verbose_name=_("Overlay layers"))
+
+    class Meta:
+        verbose_name = _("Map overlay group")
+        verbose_name_plural = _("Map overlay groups")
+        ordering = ('label',)
+
+    def __str__(self):
+        return self.label
+
+
+class MapOverlayLayer(models.Model):
+    label = models.CharField(max_length=50)
+    url = models.CharField(max_length=255, blank=False, help_text=_("URL of the layer"), unique=True)
+    default_active = models.BooleanField(default=False)
+    style = models.JSONField(max_length=300, null=False, blank=False, default=dict, help_text=_("Style of the layer"))
+    order = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        verbose_name = _("Map overlay layer")
+        verbose_name_plural = _("Map overlay layers")
+        ordering = ('label',)
+        unique_together = ('label', 'order')
+
+    def __str__(self):
+        return self.label
 
 
 class Portal(TimeStampedModelMixin, models.Model):
@@ -15,6 +68,12 @@ class Portal(TimeStampedModelMixin, models.Model):
                                    default='')
     main_color = ColorField(verbose_name=_("Main color"), default='#444444',
                             help_text=_("Main color"))
+    map_base_layers = models.ManyToManyField('portal.MapBaseLayer', verbose_name=_("Map base layers"))
+    map_group_overlay_layers = models.ManyToManyField('portal.MapOverlayGroupLayer',
+                                                      verbose_name=_("Map group overlay layers"))
+    min_zoom = models.PositiveSmallIntegerField(default=0)
+    max_zoom = models.PositiveSmallIntegerField(default=22)
+    spatial_extent = PolygonField(srid=settings.SRID, null=True)
 
     class Meta:
         verbose_name = _("Portal")
