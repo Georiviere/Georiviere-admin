b1ab2fdbbbceef9eaff2a4d427e7bb7f4342cbf8
LePetitTim <timothee.de-montety@makina-corpus.com>
2023-05-26 15:36:28 +0200

    ✅ Add tests validators

---
+++ georiviere/portal/tests/test_validators.py
@@ -0,0 +1,36 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from georiviere.portal.validators import validate_json_schema, validate_json_schema_data
+
+bad_json_schema = {
+  "type": "object",
+  "required": [
+      1
+  ],
+  "properties": {
+    "name_author": {
+      "type": "string",
+      "title": "Name author"
+    }
+  }
+}
+
+good_json_schema = {
+  "type": "object",
+  "properties": {
+    "name_author": {
+      "type": "string",
+      "title": "Name author"
+    }
+  }
+}
+
+
+class ValidateJSONSchemaTestCase(TestCase):
+    def test_validator_with_wrong_jsonschema(self):
+        with self.assertRaises(ValidationError):
+            validate_json_schema(bad_json_schema)
+
+    def test_validator_jsonschema(self):
+        self.assertEqual(good_json_schema, validate_json_schema(good_json_schema))
--- georiviere/portal/validators.py
+++ georiviere/portal/validators.py
@@ -1,7 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-from jsonschema import exceptions
-from jsonschema.validators import validator_for, validate
+from jsonschema.validators import validator_for
 
 
 def validate_json_schema(value):
@@ -14,25 +12,6 @@ def validate_json_schema(value):
             cls = validator_for(value)
             cls.check_schema(value)
     except Exception as e:
-        raise ValidationError(message=e.message)
-
-    return value
-
-
-def validate_json_schema_data(value, schema):
-    """
-    Validate data according json schema
-    """
-    try:
-        # check result schema
-        if value and schema:
-            properties = schema.get('properties').keys()
-            unexpected_properties = value.keys() - properties
-            if unexpected_properties:
-                # value key(s) not in expected properties
-                raise ValidationError(message=_(f"{unexpected_properties} not in schema properties"))
-            validate(value, schema)
-    except exceptions.ValidationError as e:
         raise ValidationError(message=e.message)
 
     return value
