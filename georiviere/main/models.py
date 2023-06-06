b8674a4d734d035e198cd9823b6cc15289c007d1
LePetitTim <timothee.de-montety@makina-corpus.com>
2023-06-06 10:56:20 +0200

    🐛 Fix attachment without creator

--- georiviere/main/models.py
+++ georiviere/main/models.py
@@ -19,6 +19,17 @@ class Attachment(BaseAttachment):
                                 verbose_name=_('Creator'),
                                 help_text=_("User that uploaded"), on_delete=models.CASCADE, blank=True, null=True)
 
+    def __str__(self):
+        if self.creator:
+            return '{} attached {}'.format(
+                self.creator.username,
+                self.attachment_file.name
+            )
+        else:
+            return 'attached {}'.format(
+                self.attachment_file.name
+            )
+
 
 class DataSource(StructureOrNoneRelated):
 
