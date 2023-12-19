5f5628a08555bafe0953a95c8ba5cdd8f613cd10
J-E Castagnede <j.e.castagnede@gmail.com>
2023-12-19 20:54:05 +0100

    use pg triggers

--- georiviere/main/sql/pre_10_types.sql
+++
@@ -1,9 +0,0 @@
-DROP TYPE IF EXISTS elevation_infos;
-CREATE TYPE elevation_infos AS (
-    draped geometry,
-    slope float,
-    min_elevation integer,
-    max_elevation integer,
-    positive_gain integer,
-    negative_gain integer
-);
