697bf9ce4eff695e4b050276a591edcda7e6b321
J-E Castagnede <j.e.castagnede@gmail.com>
2023-12-20 12:47:53 +0100

    use internal triggers

--- georiviere/river/models.py
+++ georiviere/river/models.py
@@ -1,4 +1,3 @@
-import pgtrigger
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.gis.db import models
@@ -97,37 +96,37 @@ class Stream(AddPropertyBufferMixin, TimeStampedModelMixin, WatershedPropertiesM
     class Meta:
         verbose_name = _("Stream")
         verbose_name_plural = _("Streams")
-        triggers = AltimetryMixin.Meta.triggers + [
-            pgtrigger.Trigger(
-                name="create_topologies",
-                operation=pgtrigger.Insert,
-                declare=[
-                    ('topology_morphology', 'integer'),
-                    ('topology_status', 'integer')
-                ],
-                when=pgtrigger.After,
-                func="""
-                        INSERT INTO river_topology (stream_id, start_position, end_position, qualified)
-                        VALUES (NEW.id, 0, 1, FALSE)  RETURNING id INTO topology_morphology;
-                        INSERT INTO description_morphology (topology_id, geom, description, date_insert, date_update)
-                                                    VALUES (topology_morphology, NEW.geom, '', NOW(), NOW());
-                        INSERT INTO river_topology (stream_id, start_position, end_position, qualified)
-                        VALUES (NEW.id, 0, 1, FALSE)  RETURNING id INTO topology_status;
-                        INSERT INTO description_status (topology_id, geom, regulation, referencial, description, date_insert, date_update)
-                                                    VALUES (topology_status, NEW.geom, FALSE, FALSE, '', NOW(), NOW());
-                        RETURN NEW;
-                    """
-            ),
-            pgtrigger.Trigger(
-                name="update_topologies",
-                operation=pgtrigger.UpdateOf('geom'),
-                when=pgtrigger.After,
-                func="""
-                                UPDATE river_topology SET id=id WHERE stream_id=NEW.id;
-                                RETURN NEW;
-                            """
-            )
-        ]
+        # triggers = AltimetryMixin.Meta.triggers + [
+        #     pgtrigger.Trigger(
+        #         name="create_topologies",
+        #         operation=pgtrigger.Insert,
+        #         declare=[
+        #             ('topology_morphology', 'integer'),
+        #             ('topology_status', 'integer')
+        #         ],
+        #         when=pgtrigger.After,
+        #         func="""
+        #                 INSERT INTO river_topology (stream_id, start_position, end_position, qualified)
+        #                 VALUES (NEW.id, 0, 1, FALSE)  RETURNING id INTO topology_morphology;
+        #                 INSERT INTO description_morphology (topology_id, geom, description, date_insert, date_update)
+        #                                             VALUES (topology_morphology, NEW.geom, '', NOW(), NOW());
+        #                 INSERT INTO river_topology (stream_id, start_position, end_position, qualified)
+        #                 VALUES (NEW.id, 0, 1, FALSE)  RETURNING id INTO topology_status;
+        #                 INSERT INTO description_status (topology_id, geom, regulation, referencial, description, date_insert, date_update)
+        #                                             VALUES (topology_status, NEW.geom, FALSE, FALSE, '', NOW(), NOW());
+        #                 RETURN NEW;
+        #             """
+        #     ),
+        #     pgtrigger.Trigger(
+        #         name="update_topologies",
+        #         operation=pgtrigger.UpdateOf('geom'),
+        #         when=pgtrigger.After,
+        #         func="""
+        #                         UPDATE river_topology SET id=id WHERE stream_id=NEW.id;
+        #                         RETURN NEW;
+        #                     """
+        #     )
+        # ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -251,23 +250,23 @@ class Topology(models.Model):
     class Meta:
         verbose_name = _("Topology")
         verbose_name_plural = _("Topologies")
-        triggers = [
-            pgtrigger.Trigger(
-                name="update_topology_geom",
-                operation=pgtrigger.Update | pgtrigger.Insert,
-                when=pgtrigger.After,
-                declare=[('stream_geom', 'geometry')],
-                func="""
-                    SELECT r.geom FROM river_stream r WHERE NEW.stream_id = r.id INTO stream_geom;
-                    UPDATE description_morphology
-                    SET geom = ST_LINESUBSTRING(stream_geom, NEW.start_position, NEW.end_position)
-                    WHERE topology_id = NEW.id;
-                    UPDATE description_status
-                    SET geom = ST_LINESUBSTRING(stream_geom, NEW.start_position, NEW.end_position)
-                    WHERE topology_id = NEW.id;
-                    RETURN NEW;
-                """
-            )]
+        # triggers = [
+        #     pgtrigger.Trigger(
+        #         name="update_topology_geom",
+        #         operation=pgtrigger.Update | pgtrigger.Insert,
+        #         when=pgtrigger.After,
+        #         declare=[('stream_geom', 'geometry')],
+        #         func="""
+        #             SELECT r.geom FROM river_stream r WHERE NEW.stream_id = r.id INTO stream_geom;
+        #             UPDATE description_morphology
+        #             SET geom = ST_LINESUBSTRING(stream_geom, NEW.start_position, NEW.end_position)
+        #             WHERE topology_id = NEW.id;
+        #             UPDATE description_status
+        #             SET geom = ST_LINESUBSTRING(stream_geom, NEW.start_position, NEW.end_position)
+        #             WHERE topology_id = NEW.id;
+        #             RETURN NEW;
+        #         """
+        #     )]
 
 
 Study.add_property('stations', Station.within_buffer, _("Stations"))
