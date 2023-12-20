9ddd1b659fb5d0717960054f009c96a8e5773514
J-E Castagnede <j.e.castagnede@gmail.com>
2023-12-20 14:20:09 +0100

    use internal triggers

--- georiviere/altimetry.py
+++ georiviere/altimetry.py
@@ -2,5 +2,16 @@ from geotrek.altimetry.models import AltimetryMixin as BaseAltimetryMixin
 
 
 class AltimetryMixin(BaseAltimetryMixin):
+    def reload(self):
+        # Update object's computed values (reload from database)
+        if self.pk:
+            fromdb = self.__class__.objects.get(pk=self.pk)
+            BaseAltimetryMixin.reload(self, fromdb)
+        return self
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.reload()
+    
     class Meta:
         abstract = True
