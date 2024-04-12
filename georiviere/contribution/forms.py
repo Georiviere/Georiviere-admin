7efcb6fa334fbebc99dd96e51204988a0c8d35b5
J-E Castagnede <j.e.castagnede@gmail.com>
2024-04-12 22:28:19 +0200

    add 500 template handler

--- georiviere/contribution/forms.py
+++ georiviere/contribution/forms.py
@@ -91,9 +91,9 @@ class CustomContributionFieldForm(forms.ModelForm):
             self.fields["customization"] = JSONFormField(
                 schema=schema, required=False, label=_("Customization")
             )
-            self.fields["value_type"].choices = [
-                (self.instance.value_type, self.instance.get_value_type_display())
-            ]
+            #self.fields["value_type"].choices = [
+            #    (self.instance.value_type, self.instance.get_value_type_display())
+            #]
 
     class Meta:
         model = models.CustomContributionTypeField
