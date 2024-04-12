0aeda66fa3e3e96b87fa31abc3a368c434328afb
J-E Castagnede <j.e.castagnede@gmail.com>
2024-04-12 21:49:17 +0200

    fix field behaviour

--- georiviere/contribution/forms.py
+++ georiviere/contribution/forms.py
@@ -91,7 +91,7 @@ class CustomContributionFieldForm(forms.ModelForm):
             self.fields["customization"] = JSONFormField(
                 schema=schema, required=False, label=_("Customization")
             )
-            self.fields["value_type"].disabled = True
+            #self.fields["value_type"].disabled = True
             #self.fields["value_type"].widget.attrs.update({"disabled": "disabled"})
             self.fields["value_type"].help_text = _("You can't change value type after creation. Delete and/or create another one.")
 
