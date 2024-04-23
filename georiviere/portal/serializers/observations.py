444c12535f0f52dde330990e3fbc83941235a085
J-E Castagnede <j.e.castagnede@gmail.com>
2024-04-23 18:41:31 +0200

    fix station endpoint

--- georiviere/portal/serializers/observations.py
+++ georiviere/portal/serializers/observations.py
@@ -4,24 +4,7 @@ from rest_framework_gis import serializers as geo_serializers
 from georiviere.observations.models import Station
 
 
-class StationMixin:
-    geometry = geo_serializers.GeometryField(
-        read_only=True, precision=7, source="geom_transformed"
-    )
-
-    class Meta:
-        model = Station
-        fields = (
-            "id",
-            "code",
-            "label",
-            "description",
-            "custom_contribution_types",
-            "geometry",
-        )
-
-
-class StationGeojsonSerializer(StationMixin, geo_serializers.GeoFeatureModelSerializer):
+class StationGeojsonSerializer(geo_serializers.GeoFeatureModelSerializer):
     geometry = geo_serializers.GeometryField(
         read_only=True, precision=7, source="geom_transformed"
     )
@@ -40,6 +23,18 @@ class StationGeojsonSerializer(StationMixin, geo_serializers.GeoFeatureModelSeri
         )
 
 
-class StationSerializer(StationMixin, ModelSerializer):
-    class Meta(StationMixin.Meta):
-        pass
+class StationSerializer(ModelSerializer):
+    geometry = geo_serializers.GeometryField(
+        read_only=True, precision=7, source="geom_transformed"
+    )
+
+    class Meta:
+        model = Station
+        fields = (
+            "id",
+            "code",
+            "label",
+            "description",
+            "custom_contribution_types",
+            "geometry",
+        )
